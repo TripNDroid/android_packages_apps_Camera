35f5e92b18d764ed63175a943622a21da9f5c3dd
TripNRaVeR <noerialexeew@live.nl>
2017-05-27 22:31:40 +0200

    camera: update menu looks

--- src/com/android/camera/ui/CameraControls.java
+++ src/com/android/camera/ui/CameraControls.java
@@ -1105,8 +1105,10 @@ public class CameraControls extends RotatableLayout {
         };
         for (View v : views) {
             if (v != null) {
-                ((RotateImageView) v).setOrientation(orientation,
-                                                     animation);
+                if (v instanceof RotateImageView) {
+                    ((RotateImageView) v).setOrientation(orientation,
+                                                         animation);
+                }
             }
         }
         layoutRemaingPhotos();
